756f98d8d5d1da5cee45f3cd3817f7d302049ca0
Michael Smith <s5062849@bournemouth.ac.uk>
2018-05-21 09:25:02 +0100

    Audio Complete

---
+++ Assets/PlaySoundFX.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySoundFX : MonoBehaviour {
+
+
+    private AudioSource source;
+    [SerializeField] private AudioClip hitSound;
+
+    private void Awake()
+    {
+        {
+            source = gameObject.GetComponent<AudioSource>();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Untagged" || collision.gameObject.tag == "CamChange")
+        {
+            source.Play();
+        }
+
+
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        source.Stop();
+    }
+}
---
+++ Assets/SoundFx.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundFx : MonoBehaviour {
+
+    private AudioSource source;
+    [SerializeField] private AudioClip hitSound;
+
+    private void Awake()
+    {
+        {
+            source = gameObject.GetComponent<AudioSource>();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Untagged" || collision.gameObject.tag == "CamChange")
+        {
+            source.PlayOneShot(hitSound, 1f);
+        }
+        
+    }
+
+
+
+
+}
